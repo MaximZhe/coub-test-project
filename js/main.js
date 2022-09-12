1cda7a7c8a683226ae23787f16285b529f9490d1
max <sen989@yandex.ru>
2022-09-12 13:00:29 +0700

    Final setting

--- js/main.js
+++ js/main.js
@@ -2,27 +2,31 @@
 const logo = document.querySelector(".logo");
 const logoText = document.querySelector(".logo__text");
 const btns = document.querySelectorAll(".projects__btn");
+const btnMenuOpen = document.querySelector("#namburger");
+const modal = document.querySelector("#menu-mobail");
+const body = document.querySelector("body");
+
 //Перебираем кнопки и обрабатываем событие мыши.
 btns.forEach(item => {
     console.log(item.children[0])
     item.addEventListener("mouseover", (e) => {
         if(e.target === item){
-          arrowActive (item.children[0])  
+            classActive (item.children[0])  
         }
     })
     item.addEventListener("mouseout", (e) => {
         if(e.target === item){
-            arrowDeactivation (item.children[0])  
+            classDeactivation (item.children[0])  
         }
     })
     
 })
 //Добавляем активный класс стрелке
-function arrowActive (item){
+function classActive (item){
     item.classList.add("active");
 }
 //Убираем активный класс у стрелки
-function arrowDeactivation (item){
+function classDeactivation (item){
     item.classList.remove("active");
 }
 
@@ -32,3 +36,16 @@ logo.addEventListener("mouseover", () =>{
 logo.addEventListener("mouseout", () =>{
     logoText.style.color ="#383DB7";
 })
+// Открыаем и закрываем меню в телефоне
+btnMenuOpen.addEventListener("click", () => {
+    if(!btnMenuOpen.classList.contains("active")){
+        classActive(modal);
+        body.style.paddingTop = "200px";
+        btnMenuOpen.classList.add("active"); 
+    }else if(modal.classList.contains("active")){
+        classDeactivation (modal);
+        body.style.paddingTop = "0px";
+        btnMenuOpen.classList.remove("active");
+    }  
+})
+
